2e1a44648f9bf89d9965f38c8bef97cc9d7aea04
ItzProxy <allure.success@gmail.com>
2018-02-01 11:08:55 -0600

    Add files via upload
    sktetch_feb01a - 2 Button that act as a switch for a single led
    sketch_feb01b - 2 buttons that turn off the other led
    sketch_feb01c - 1 button that iterates through 4 different led modes

---
+++ sketch_feb01b/sketch_feb01b.ino
@@ -0,0 +1,29 @@
+const int buttonPin1 = 2;       // pushbutton pin
+const int buttonPin2 = 3;       //other pushbutton pin
+const int led1 =  9;            // LED  pin
+const int led2 = 10;
+int buttonState = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+     
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(buttonPin1) == LOW){ //checks if button 1 is pressed
+    digitalWrite(led1, HIGH); //turn on led
+    digitalWrite(led2, LOW); //turn on led
+  }
+  else if(digitalRead(buttonPin2) == HIGH){ //check if button 2 is pressed 
+    digitalWrite(led1, LOW); //turn off led
+    digitalWrite(led2, HIGH); //turn on led
+  }
+  else{
+    
+  }
+}
