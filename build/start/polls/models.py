15c30b2a59f8caaed885260c38534d0b9c6f1f99
ItzProxy <allure.success@gmail.com>
2018-04-16 18:55:54 -0600

    Finishing off paper stuff
    Documentation of FSM and mock ups of designs for project

--- build/start/polls/models.py
+++ build/start/polls/models.py
@@ -26,3 +26,4 @@ class Fuck(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
